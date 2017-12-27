ca1c2edcb6f3468c0ef85ff3613d6b2fba06aebd
Vlad Minzatu <minzatu.vlad@gmail.com>
2017-12-27 20:35:02 +0200

    outline of main endpoints

--- src/main/scala/com/github/vladminzatu/providentia/app/ProvicentiaApp.scala
+++ src/main/scala/com/github/vladminzatu/providentia/app/ProvicentiaApp.scala
@@ -13,9 +13,43 @@ object ProvicentiaApp {
     implicit val materializer = ActorMaterializer()
 
     val route =
-      path("health") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "OK"))
+      pathPrefix("health") {
+        pathEndOrSingleSlash {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "OK"))
+          }
+        }
+      } ~ pathPrefix("references") {
+        pathEndOrSingleSlash {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "List of all references"))
+          } ~ post {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Create a new reference"))
+          }
+        }
+      } ~ pathPrefix("references" / Segment) { id =>
+        pathEndOrSingleSlash {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"Reference with id $id"))
+          } ~ put {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"Update reference with id $id"))
+          }
+        }
+      } ~ pathPrefix("tags") {
+        pathEndOrSingleSlash {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "List of all tags"))
+          } ~ post {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Create a new tag"))
+          }
+        }
+      } ~ pathPrefix("tags" / Segment) { id =>
+        pathEndOrSingleSlash {
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"Tag with id $id"))
+          } ~ put {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"Update tag with $id"))
+          }
         }
       }
 
